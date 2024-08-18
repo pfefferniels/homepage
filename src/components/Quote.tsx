9e64d7fa93aa73c5bbbcfe85233384f37d07cda5
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2024-08-18 14:31:02 +0200

    fix quote

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export const Quote = ({ children }: { children: ReactNode }) => {
     return (
         <>
-            ‹{children}›
+            »{children}«
         </>
     )
 }
